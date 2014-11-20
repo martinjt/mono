fad5c59dcbb4cb97faa4c427aea0b9d754816c27
Martin Thwaites <github@my2cents.co.uk>
2014-11-20 23:21:05 +0000

    Replaced more files

---
+++ mcs/class/System.Web/System.Web.Hosting/IIS7WorkerRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace System.Web.Hosting
+{
+	public class IIS7WorkerRequest : HttpWorkerRequest
+	{
+		public IIS7WorkerRequest()
+		{
+		}
+
+		public string MapHandlerAndGetHandlerTypeString(string str, string str2, bool b, bool b2)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ mcs/class/System.Web/System.Web.Hosting/ISAPIApplicationHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace System.Web.Hosting
+{
+	public class ISAPIApplicationHost : MarshalByRefObject, IApplicationHost
+	{
+		public ISAPIApplicationHost()
+		{
+		}
+
+		public string ResolveRootWebConfigPath()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IProcessHostSupportFunctions SupportFunctions()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
